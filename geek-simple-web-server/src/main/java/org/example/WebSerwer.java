b2135a69d82d0c61916aa2b761fd56725b135b9d
1chello <underzero@bk.ru>
2023-03-03 18:24:26 +0300

    add geek simple web server

---
+++ geek-simple-web-server/src/main/java/org/example/WebSerwer.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class WebSerwer {
+    public static void main(String[] args) {
+        try (ServerSocket ServerSocket = new ServerSocket(8088)) {
+
+            while (true) {
+                Socket Socket = ServerSocket.accept();
+                System.out.println("New client connected!");
+
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(
+                                Socket.getInputStream(), StandardCharsets.UTF_8));
+                PrintWriter output = new PrintWriter(Socket.getOutputStream());
+
+                while (!input.ready()) ;
+
+                while (input.ready()) {
+                    System.out.println(input.readLine());
+                }
+
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html charset=utf-8");
+                output.println();
+                output.println("<h1>Hello from server!</h1>");
+                output.flush();
+
+                input.close();
+                output.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
